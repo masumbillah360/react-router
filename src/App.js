1443ffe77db42e73e81f126e1bc6331046ed59a0
masumbillah360 <masumbillah6584@gmail.com>
2022-10-03 22:21:38 +0600

    added page not found page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-
-import './App.css';
-import Home from './componenets/Home/Home';
-import About from './componenets/About/About';
-import Blog from './componenets/Blog/Blog';
-import Layout from './Layout/Layout';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import About from "./componenets/About/About";
+import Blog from "./componenets/Blog/Blog";
+import Home from "./componenets/Home/Home";
+import Notfound from "./componenets/Notfound/Notfound";
+import Layout from "./Layout/Layout";
 
 function App() {
-  const router = createBrowserRouter([
-    {path : "/", element: <Layout></Layout>, children : [
-      {path : "/home", element: <Home></Home>},
-      {path : "/blog", element: <Blog></Blog>},
-      {path : "/about", element: <About></About>},
-    ]},
-  ]);
-  return (
-    <div className="App">
-      <RouterProvider router={router}></RouterProvider>
-    </div>
-  );
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Layout></Layout>,
+			children: [
+				{ path: "/home", element: <Home></Home> },
+				{ path: "/blog", element: <Blog></Blog> },
+				{ path: "/about", element: <About></About> },
+			],
+		},
+    {path : "*", element: <Notfound></Notfound>}
+	]);
+	return (
+		<div className="App">
+			<RouterProvider router={router}></RouterProvider>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/componenets/Notfound/Notfound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notfound = () => {
+    return (
+        <div className='flex justify-center items-center min-h-screen bg-gray-700'>
+            <h1 className='text-gray-500 font-bold text-9xl'>Page Not Found</h1>
+        </div>
+    );
+};
+
+export default Notfound;
